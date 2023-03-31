9ddfc6e36c9004b2f130ad453ba98893de58a221
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-03-31 15:54:31 +0900

    feat: setup SharedTranslateViewModel and AndroidTranslateViewModel

---
+++ shared/src/commonMain/kotlin/com/kenshi/kmmtranslator/translate/presentation/TranslateState.kt
@@ -0,0 +1,16 @@
+package com.kenshi.kmmtranslator.translate.presentation
+
+import com.kenshi.kmmtranslator.core.presentation.UiLanguage
+import com.kenshi.kmmtranslator.translate.domain.translate.TranslateError
+
+data class TranslateState(
+    val fromText: String = "",
+    val toText: String? = null,
+    val isTranslating: Boolean = false,
+    val fromLanguage: UiLanguage = UiLanguage.byCode("en"),
+    val toLanguage: UiLanguage = UiLanguage.byCode("de"),
+    val isChoosingFromLanguage: Boolean = false,
+    val isChoosingToLanguage: Boolean = false,
+    val error: TranslateError? = null,
+    val history: List<UiHistoryItem> = emptyList()
+)
