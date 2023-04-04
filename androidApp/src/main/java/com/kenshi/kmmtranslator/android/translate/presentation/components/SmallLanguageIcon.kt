5fedd7b3015ae8b99cc57aba7c087aa929265829
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-04-04 11:26:49 +0900

    feat: Translation TextField 구성

---
+++ androidApp/src/main/java/com/kenshi/kmmtranslator/android/translate/presentation/components/SmallLanguageIcon.kt
@@ -0,0 +1,21 @@
+package com.kenshi.kmmtranslator.android.translate.presentation.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.kenshi.kmmtranslator.core.presentation.UiLanguage
+
+//TODO UiLanguage 의 존재 이유
+@Composable
+fun SmallLanguageIcon(
+    language: UiLanguage,
+    modifier: Modifier = Modifier
+) {
+    AsyncImage(
+        model = language.drawableRes,
+        contentDescription = language.language.langName,
+        modifier = modifier.size(25.dp)
+    )
+}
